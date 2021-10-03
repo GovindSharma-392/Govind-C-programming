498e7b18d5a5fc38c18f85d5e32c138f174ef49d
GovindSharma-392 <91676467+GovindSharma-392@users.noreply.github.com>
2021-10-03 15:29:20 +0530

    Add files via upload

---
+++ govindQ6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int divid,divis,quot,rem;
+    divid=9;
+    divis=2;
+    
+    quot=divid/divis;
+    rem=divid%divis;
+    
+    printf("quot=%d\n",quot);
+    printf("rem=%d",rem);
+    
+    
+    return 0;
+    
+}
+
+
+
+
+
