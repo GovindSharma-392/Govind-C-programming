7482040880fbf9aa5457da881d9feca7aef4534e
GovindSharma-392 <91676467+GovindSharma-392@users.noreply.github.com>
2021-10-14 19:22:47 +0530

    Add files via upload

---
+++ govindQ34.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{ 
+    int add (int x,int y);
+
+    scanf("%d%d",&x,&y);
+    printf("sum = %d",add(3,4));
+    return 0;
+}
---
+++ govindQ43.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float fv=50,iv=30,a=2.5,t;
+    t=(fv-iv)/a;
+    printf("t=%d",t);
+    
+    return 0;
+}
